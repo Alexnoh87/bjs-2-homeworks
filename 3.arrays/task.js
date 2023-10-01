48e1852f25096862617f2dd219a0b163d91a71ca
Alex Nohrin <beterok@inbox.ru>
2023-10-01 18:26:22 +0500

    Update third task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -4,12 +4,8 @@ function compareArrays(arr1, arr2) {
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-    let result = users.filter(user => user.gender === gender).map(item => item.age).reduce((acc, item, index, arr) => {
-        acc+=item;
-        if(index === arr.length -1) {
-          return acc / arr.length
-        }
-        return acc;
-    }, 0)
+    let result = users.filter(user => user.gender === gender)
+    .map(user => user.age)
+    .reduce((acc, age, index, arr) => acc + age / arr.length, 0);
     return result;
 }
